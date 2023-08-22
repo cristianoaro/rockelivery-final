6e709bf6bafc6ebedd6e5b8b40ed7ec4fc8be698
Cristiano Aro <cristiano_sx@hotmail.com>
2023-08-22 18:16:04 -0300

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,51 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.Item
+alias Rockelivery.Order
+alias Rockelivery.Repo
+alias Rockelivery.User
+
+user = %User{
+  cpf: "12345678902",
+  email: "testew@teste.com",
+  name: "Teste 01",
+  address: "Rua dos testes",
+  password: "123456",
+  age: 28,
+  cep: "06727000"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1 = %Item{
+  category: "food",
+  description: "banana",
+  price: Decimal.new("1.99"),
+  photo: "priv/photos/banana.jpg"
+}
+
+item2 = %Item{
+  category: "food",
+  description: "banana frita",
+  price: Decimal.new("4.99"),
+  photo: "priv/photos/banana.jpg"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [
+    item1,
+    item2,
+    item2
+  ],
+  address: "Rua dos Testadores, 1234, Testana",
+  comments: "Teste 01 order",
+  payment_method: :money
+}
+
+Repo.insert!(order)
